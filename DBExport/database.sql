b2d9446f7077e09833771e089a028cc67240b74a
Febrian-chiperbase <fery.pradana@binus.ac.id>
2025-02-07 19:03:54 +0700

    Update database.sql

--- DBExport/database.sql
+++ DBExport/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 1, 2025 at 09:12 PM
+-- Generation Time: Feb 01, 2025 at 09:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
