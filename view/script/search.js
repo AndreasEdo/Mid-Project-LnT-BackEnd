ef075a8af3e622f8f00bd0b5e8a621a5393002c8
AndreasEdo <andreasedwardputrajatmiko@gmail.com>
2025-02-08 22:23:26 +0700

    Fix search

--- view/script/search.js
+++ view/script/search.js
@@ -37,21 +37,35 @@ document.addEventListener('DOMContentLoaded', function () {
     
         visibleUsers.forEach((user, index) => {
             tableHTML += `
-                <tr>
-                    <th scope="row">${index + 1}</th>
-                    <td class="d-flex justify-content-center">
-                        <img src="${user.photo ? '../img/' + user.photo : 'https://via.placeholder.com/180x150'}" 
-                             alt="profile_picture" 
-                             style="width: 100px; border-radius: 50%; height:100px;">
-                    </td>
-                    <td>${user.first_name} ${user.last_name}</td>
-                    <td>${user.email}</td>
-                    <td>
-                        <a href="#" class="btn btn-primary btn-md">View</a>
-                        <a href="editUser.php?id=${user.id}" class="btn btn-warning btn-md">Edit</a>
-                        <a href="#" class="btn btn-danger btn-md">Delete</a>
-                    </td>
-                </tr>
+            <tr>
+                <th scope="row"><?php echo $num++?></th>
+                <td class="d-flex justify-content-center">
+                    <img src="<?= empty($user['photo']) ? 'https://via.placeholder.com/180x150' : '../img/'. $user['photo']?>" 
+                        alt="profile_picture" 
+                        style="width: 100px; border-radius: 50%; height:100px;">
+                </td>
+                <td><?php echo $user['first_name'].' '. $user['last_name']?> </td>
+                <td><?php echo $user['email']?> </td>
+                <td>
+                    <a href="detailuser.php?id=<?= urlencode($user['id']) ?>" class="btn btn-primary btn-md">View</a>
+                    <a href="editUser.php?id=<?= $user['id'] ?>" class="btn btn-warning btn-md">Edit</a>
+                    <button class="btn btn-danger btn-md" onclick="openModal('warningRemove<?= $user['id'] ?>')">Remove</button>
+                </td>
+            </tr>
+            <div class="warning"  id="warningRemove<?= $user['id'] ?>" >
+                <div class="warning-wrapper" style="text-align: left">
+                    <form method="POST">
+                        <input type="hidden" name="id" value="<?= $user["id"] ?>">
+                        <input type="hidden" name="photo" value="<?= $user["photo"] ?>">
+                        <img src="assets/warning-icon.png" class="warning-icon">
+                        <h5 class="h5-warning">Are you sure you want to delete</h5><h5 class="warning-name"><?php echo $user['first_name'].' '. $user['last_name']?></h5>
+                        <div class="button-wrapper">
+                            <button type="submit" class="btn btn-danger" name="deleteBtn">Yes</button>
+                            <button type="button" class="btn btn-secondary" onclick="closeModal('warningRemove<?= $user['id'] ?>')">No</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
             `;
         });
     
